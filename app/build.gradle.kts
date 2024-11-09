d9d73f52b4a0960a557a4d4ba76ff0072ac9eda6
Niraviman-Singha <niraviman15-11689@diu.edu.bd>
2024-11-10 00:10:42 +0600

    Text Recognition OCR App frontend design done.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.textrecognitionocrapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.textrecognitionocrapp"
@@ -44,4 +44,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    implementation ("com.google.mlkit:text-recognition:16.0.1")
 }
